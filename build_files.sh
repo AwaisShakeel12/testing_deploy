c4c14892eb8dff90a8a7b90290f35946339f776c
AwaisShakeel12 <awais.ok612@gmail.com>
2025-03-21 11:30:00 +0500

    change vercel.json and build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+# pip install -r requirements.txt
+# python3.9 manage.py collectstatic
+
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
